d09a0c9866f45a78faa039f270e739c5a72f369f
leovolving <leo@leoyockey.com>
2025-07-05 18:56:39 -0400

    handle search input param

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,8 +1,24 @@
+import { NextRequest } from "next/server";
+import { ilike, or } from "drizzle-orm";
+
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 
-export async function GET() {
-  const data = await db.select().from(advocates);
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const searchInput = searchParams.get("search")?.trim();
+  const data = searchInput
+    ? await db
+        .select()
+        .from(advocates)
+        .where(
+          or(
+            // TODO: handle partial matches
+            ilike(advocates.firstName, searchInput),
+            ilike(advocates.lastName, searchInput)
+          )
+        )
+    : await db.select().from(advocates);
 
   return Response.json({ data });
 }
